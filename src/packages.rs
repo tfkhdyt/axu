50d9d51e8c727c9d88d3295fb049f2d98b47b961
tfkhdyt <me@tfkhdyt.my.id>
2023-10-15 08:02:27 +0700

    fix(get_explicit_packages): don't need to sort output

--- src/packages.rs
+++ src/packages.rs
@@ -7,7 +7,6 @@ use crate::utils::parsing;
 pub fn get_explicit_packages() -> io::Result<Vec<String>> {
     let explicit_packages = cmd!("yay", "-Qe")
         .pipe(cmd!("awk", "-F", " ", "{print $1}"))
-        .pipe(cmd!("sort"))
         .read()?;
     let splited_packages =
         parsing::vec_str_to_vec_string(explicit_packages.split('\n').collect::<Vec<&str>>());
