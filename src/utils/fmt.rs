bbf61b36230ce30f33b7f69a7d4caff2445519f7
tfkhdyt <me@tfkhdyt.my.id>
2023-10-17 22:22:42 +0700

    chore: remove fatalln

--- src/utils/fmt.rs
+++
@@ -1,9 +0,0 @@
-use std::{error::Error, process};
-
-pub fn fatalln(message: &str, err: Option<&dyn Error>) -> ! {
-    eprintln!("Error: {}", message);
-    if let Some(err) = err {
-        eprintln!("{}", err);
-    }
-    process::exit(1)
-}
