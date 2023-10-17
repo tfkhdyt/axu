f887e58cce91a7eee8de0a582803ab6b4dd28216
tfkhdyt <me@tfkhdyt.my.id>
2023-10-17 22:24:49 +0700

    fix(updates): uncheck yay -Qua when returning exit code non zero

--- src/updates.rs
+++ src/updates.rs
@@ -1,12 +1,5 @@
-use std::{
-    io,
-    sync::{Arc, Mutex},
-};
-
+use anyhow::Context;
 use clap::ValueEnum;
-use rayon::prelude::*;
-
-use crate::utils::parsing;
 
 #[derive(ValueEnum, Clone, PartialEq, PartialOrd)]
 pub enum UpdateType {
@@ -17,25 +10,25 @@ pub enum UpdateType {
     Git,
 }
 
-pub fn get_all_updates() -> io::Result<Vec<String>> {
-    let poison_error_to_io_error =
-        |e| io::Error::new(io::ErrorKind::Other, format!("mutex lock error: {:?}", e));
+pub fn get_all_updates() -> anyhow::Result<Vec<String>> {
+    let (arch_updates, aur_updates) = rayon::join(
+        || -> anyhow::Result<_> {
+            duct::cmd!("checkupdates")
+                .read()
+                .context("failed to get Arch Linux updates")
+        },
+        || -> anyhow::Result<_> {
+            duct::cmd!("yay", "-Qua")
+                .unchecked()
+                .read()
+                .context("failed to get AUR updates")
+        },
+    );
 
-    let updates = Arc::new(Mutex::new(vec![]));
-    let commands = [duct::cmd!("checkupdates"), duct::cmd!("yay", "-Qua")];
+    let all_updates = format!("{}\n{}", arch_updates?, aur_updates?);
 
-    commands.par_iter().try_for_each(|cmd| -> io::Result<()> {
-        let update = cmd.read()?;
-        updates
-            .lock()
-            .map_err(poison_error_to_io_error)?
-            .push(update);
-        Ok(())
-    })?;
+    let mut splited_vec: Vec<String> = all_updates.split('\n').map(|s| s.to_owned()).collect();
 
-    let all_updates = updates.lock().map_err(poison_error_to_io_error)?.join("\n");
-    let mut splited_vec =
-        parsing::vec_str_to_vec_string(all_updates.split('\n').collect::<Vec<&str>>());
     splited_vec.sort();
 
     Ok(splited_vec)
