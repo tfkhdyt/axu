a1acd6f6853e0ca9033c2a5b1c52052fdc7c4c4a
tfkhdyt <me@tfkhdyt.my.id>
2023-11-06 21:44:00 +0700

    perf(lines): don't use par_iter too much

--- src/utils/lines.rs
+++ src/utils/lines.rs
@@ -1,12 +1,9 @@
-use std::{thread, time::Duration};
-
 use indicatif::ProgressBar;
-use rayon::prelude::*;
 
 pub fn get_common_lines(left: &[String], right: &[String], pb: &ProgressBar) -> Vec<String> {
     pb.set_message("fetch explicit updates");
     let common_lines: Vec<String> = left
-        .par_iter()
+        .iter()
         .filter_map(|x| {
             let string_vec = x.split_whitespace().collect::<Vec<&str>>();
             if let Some(first) = string_vec.first() {
@@ -21,7 +18,6 @@ pub fn get_common_lines(left: &[String], right: &[String], pb: &ProgressBar) -> 
         })
         .collect();
     pb.inc(1);
-    thread::sleep(Duration::from_millis(100));
 
     common_lines
 }
